f5bdc55a1a2bd740540430858695a7990646443c
Higor Moura Gomes <70344407+Higormourag25@users.noreply.github.com>
2020-09-16 16:41:29 -0300

    Add files via upload

---
+++ CRUD/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace CRUD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.txt_Nome = new System.Windows.Forms.TextBox();
+            this.txt_Id = new System.Windows.Forms.TextBox();
+            this.brnNovo = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnConsult = new System.Windows.Forms.Button();
+            this.btnExibir = new System.Windows.Forms.Button();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.dgvDados = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDados)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txt_Nome
+            // 
+            this.txt_Nome.Location = new System.Drawing.Point(93, 66);
+            this.txt_Nome.Name = "txt_Nome";
+            this.txt_Nome.Size = new System.Drawing.Size(100, 20);
+            this.txt_Nome.TabIndex = 0;
+            // 
+            // txt_Id
+            // 
+            this.txt_Id.AccessibleName = "";
+            this.txt_Id.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.txt_Id.Location = new System.Drawing.Point(93, 29);
+            this.txt_Id.Multiline = true;
+            this.txt_Id.Name = "txt_Id";
+            this.txt_Id.Size = new System.Drawing.Size(100, 20);
+            this.txt_Id.TabIndex = 1;
+            this.txt_Id.TextChanged += new System.EventHandler(this.txt_Id_TextChanged);
+            // 
+            // brnNovo
+            // 
+            this.brnNovo.Location = new System.Drawing.Point(37, 130);
+            this.brnNovo.Name = "brnNovo";
+            this.brnNovo.Size = new System.Drawing.Size(75, 23);
+            this.brnNovo.TabIndex = 2;
+            this.brnNovo.Text = "Novo";
+            this.brnNovo.UseVisualStyleBackColor = true;
+            this.brnNovo.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.Location = new System.Drawing.Point(199, 130);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(75, 23);
+            this.btnEditar.TabIndex = 3;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = true;
+            this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click);
+            // 
+            // btnConsult
+            // 
+            this.btnConsult.Location = new System.Drawing.Point(118, 130);
+            this.btnConsult.Name = "btnConsult";
+            this.btnConsult.Size = new System.Drawing.Size(75, 23);
+            this.btnConsult.TabIndex = 4;
+            this.btnConsult.Text = "Consultar";
+            this.btnConsult.UseVisualStyleBackColor = true;
+            this.btnConsult.Click += new System.EventHandler(this.btnConsult_Click);
+            // 
+            // btnExibir
+            // 
+            this.btnExibir.Location = new System.Drawing.Point(280, 130);
+            this.btnExibir.Name = "btnExibir";
+            this.btnExibir.Size = new System.Drawing.Size(75, 23);
+            this.btnExibir.TabIndex = 5;
+            this.btnExibir.Text = "Exibir ";
+            this.btnExibir.UseVisualStyleBackColor = true;
+            this.btnExibir.Click += new System.EventHandler(this.btnExibir_Click);
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.Location = new System.Drawing.Point(361, 130);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(75, 23);
+            this.btnExcluir.TabIndex = 6;
+            this.btnExcluir.Text = "Excluir ";
+            this.btnExcluir.UseVisualStyleBackColor = true;
+            this.btnExcluir.Click += new System.EventHandler(this.btnExcluir_Click);
+            // 
+            // dgvDados
+            // 
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvDados.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgvDados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvDados.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dgvDados.Location = new System.Drawing.Point(37, 178);
+            this.dgvDados.Name = "dgvDados";
+            this.dgvDados.Size = new System.Drawing.Size(399, 150);
+            this.dgvDados.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(68, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 17);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Id:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(42, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 17);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Nome:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(455, 352);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgvDados);
+            this.Controls.Add(this.btnExcluir);
+            this.Controls.Add(this.btnExibir);
+            this.Controls.Add(this.btnConsult);
+            this.Controls.Add(this.btnEditar);
+            this.Controls.Add(this.brnNovo);
+            this.Controls.Add(this.txt_Id);
+            this.Controls.Add(this.txt_Nome);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.Text = "Crud";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDados)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_Nome;
+        private System.Windows.Forms.TextBox txt_Id;
+        private System.Windows.Forms.Button brnNovo;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Button btnConsult;
+        private System.Windows.Forms.Button btnExibir;
+        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.DataGridView dgvDados;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ CRUD/Form1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data.Common;
+
+namespace CRUD
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection conexao;
+        SqlCommand comando;
+        SqlDataAdapter Da;
+        SqlDataReader Dr;
+
+        string strSQL;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                //Para este crud funcionar ensira o endereço do seu servidor local, sua base de dados e usuario do seu sql;
+                //E crie uma tabela com os campos id e nome;
+                conexao = new SqlConnection(@"Server ;Database=USUARIO; User Id=; Password=;");
+                strSQL = "INSERT INTO CLIENTE (NOME) VALUES (@NOME)";
+
+                comando = new SqlCommand(strSQL, conexao);
+
+                comando.Parameters.AddWithValue("@NOME", txt_Nome.Text);
+
+                conexao.Open(); 
+                comando.ExecuteNonQuery();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally 
+            {
+                conexao.Close();
+                conexao = null;
+                comando = null;
+            }
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnExcluir_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                //Para este crud funcionar ensira o endereço do seu servidor local, sua base de dados e usuario do seu sql;
+                //E crie uma tabela com os campos id e nome;
+                 conexao = new SqlConnection(@"Server= ;Database=; User Id=sa; Password=;");
+                strSQL = "DELETE CLIENTE WHERE ID = @ID";
+
+                comando = new SqlCommand(strSQL, conexao);
+
+                comando.Parameters.AddWithValue("@ID", txt_Id.Text);
+
+                conexao.Open();
+                comando.ExecuteNonQuery();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conexao.Close();
+                conexao = null;
+                comando = null;
+            }
+        }
+
+        private void btnExibir_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                //Para este crud funcionar ensira o endereço do seu servidor local, sua base de dados e usuario do seu sql;
+                //E crie uma tabela com os campos id e nome;
+                conexao = new SqlConnection(@"Server= ;Database=; User Id=sa; Password=;");
+                strSQL = "SELECT * FROM CLIENTE";
+
+                DataSet ds = new DataSet();
+
+                Da = new SqlDataAdapter(strSQL, conexao);
+
+                conexao.Open();
+
+                Da.Fill(ds);
+
+                dgvDados.DataSource = ds.Tables[0];
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conexao.Close();
+                conexao = null;
+                comando = null;
+            }
+
+        }
+
+        private void btnConsult_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                //Para este crud funcionar ensira o endereço do seu servidor local, sua base de dados e usuario do seu sql;
+                //E crie uma tabela com os campos id e nome;
+                conexao = new SqlConnection(@"Server= ;Database=; User Id=sa; Password= ;");
+                strSQL = "SELECT * FROM CLIENTE WHERE ID = @ID";
+
+                comando = new SqlCommand(strSQL, conexao);
+
+                comando.Parameters.AddWithValue("@ID", txt_Id.Text);
+
+                conexao.Open();
+                Dr = comando.ExecuteReader();
+
+                while (Dr.Read()) 
+                {
+                    txt_Nome.Text = (string)Dr["NOME"];
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conexao.Close();
+                conexao = null;
+                comando = null;
+            }
+
+
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                //Para este crud funcionar ensira o endereço do seu servidor local, sua base de dados e usuario do seu sql;
+                //E crie uma tabela com os campos id e nome;
+                conexao = new SqlConnection(@"Server=;Database=USUARIO; User Id=; Password=;");
+                strSQL = "UPDATE CLIENTE SET NOME = @NOME WHERE ID = @ID  ";
+
+                comando = new SqlCommand(strSQL, conexao);
+
+                comando.Parameters.AddWithValue("@NOME", txt_Nome.Text);
+                comando.Parameters.AddWithValue("@ID", txt_Id.Text);
+
+                conexao.Open();
+                comando.ExecuteNonQuery();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conexao.Close();
+                conexao = null;
+                comando = null;
+            }
+
+
+        }
+
+        private void txt_Id_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
